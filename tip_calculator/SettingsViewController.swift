7388939a7b2c7dbdb3536dd1309fc1f8ac85a0b5
Albert Ai <gsbalbert2002@gmail.com>
2021-07-10 15:28:29 -0700

    Added settings screen

---
+++ tip_calculator/SettingsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  SettingsViewController.swift
+//  tip_calculator
+//
+//  Created by Albert Ai on 7/9/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var tipOne: UITextField!
+    @IBOutlet weak var tipTwo: UITextField!
+    @IBOutlet weak var tipThree: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Loading previously saved tip percentages
+        let defaults = UserDefaults.standard
+        tipOne.text = defaults.string(forKey: "tip1")
+        tipTwo.text = defaults.string(forKey: "tip2")
+        tipThree.text = defaults.string(forKey: "tip3")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Saving tip data
+        let defaults = UserDefaults.standard
+        defaults.set(tipOne.text, forKey: "tip1")
+        defaults.set(tipTwo.text, forKey: "tip2")
+        defaults.set(tipThree.text, forKey: "tip3")
+        defaults.synchronize()
+    }
+}
--- tip_calculator/ViewController.swift
+++ tip_calculator/ViewController.swift
@@ -16,15 +16,54 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Sets the title in the navigation bar
+        self.title = "Tip Calculator"
+        
+        // Sets up default tip percentages
+        let defaults = UserDefaults.standard
+        if defaults.integer(forKey: "tip1") == 0 {
+            defaults.set("15", forKey: "tip1")
+        }
+        if defaults.integer(forKey: "tip2") == 0 {
+            defaults.set("18", forKey: "tip2")
+        }
+        if defaults.integer(forKey: "tip3") == 0 {
+            defaults.set("20", forKey: "tip3")
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Sets up the tip percentages text from UserDefaults
+        let defaults = UserDefaults.standard
+        tipControl.setTitle(
+            "\(defaults.string(forKey: "tip1")!)%",
+            forSegmentAt: 0
+        )
+        tipControl.setTitle(
+            "\(defaults.string(forKey: "tip2")!)%",
+            forSegmentAt: 1
+        )
+        tipControl.setTitle(
+            "\(defaults.string(forKey: "tip3")!)%",
+            forSegmentAt: 2
+        )
+        
+        calculateTip(1)
     }
     
     @IBAction func calculateTip(_ sender: Any) {
+        let defaults = UserDefaults.standard
+        
         // Get bill amount from text field input
         let bill = Double(billAmountTextField.text!) ?? 0
         
         // Get total tip by multiplying tip * tipPercentage
-        let tipPercentages = [0.15, 0.18, 0.2]
+        let tipPercentages = [
+            defaults.double(forKey: "tip1") * 0.01,
+            defaults.double(forKey: "tip2") * 0.01,
+            defaults.double(forKey: "tip3") * 0.01,
+        ]
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         let total = bill + tip
         
@@ -33,6 +72,10 @@ class ViewController: UIViewController {
         
         // Update total amount
         totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func onBillChanged(_ sender: Any) {
+        calculateTip(sender)
     }
 }
 
